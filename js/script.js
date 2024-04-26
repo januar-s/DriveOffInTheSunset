e1d2e687b61cc4b42ac5a9c375d4297f46e371ee
januar-s <jan.schmidt@stud.fhgr.ch>
2024-04-26 14:12:28 +0200

    Setup API Zugriff
    HTML aufgesetzt
    CSS aufgesetzt
    JS mit API verbunden

---
+++ js/script.js
@@ -0,0 +1,45 @@
+// Diese Elemente will ich immer wieder verwenden
+// querySelector() -> #gibt für id und . für class
+const Staedte = document.querySelector('#stadt_suche');
+const sonnenaufgang = document.querySelector('#sonnenaufgang');
+const sonnenstand = document.querySelector('#sonnenstand');
+const sonnenuntergang = document.querySelector('#sonnenuntergang');
+let url = 'https://api.sunrise-sunset.org/json?lat=46.94798&lng=7.44743&date=today';
+
+// // Initalisierung
+// init();
+
+// async function init() {
+//     let cocktails = await fetchData(url);
+//     console.log(cocktails.drinks[0].strDrink);
+//     cocktails.drinks.forEach(cocktail => {
+//         createItem(cocktail);
+//     });
+// };
+
+// async function search() {
+//     let searchValue = searchBox.value;
+//     let url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchValue}`;
+//     cocktailApp.innerHTML = '';
+//     let cocktails = await fetchData(url);
+//     console.log(cocktails.drinks[0].strDrink);
+//     cocktails.drinks.forEach(cocktail => {
+//         createItem(cocktail);
+//     });
+
+// }
+
+// Daten aus einer API holen
+async function fetchData(url) {
+    try {
+        let response = await fetch(url);
+        let data = await response.json();
+        console.log(data);
+        return data;
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+fetchData(url);
