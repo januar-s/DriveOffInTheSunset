2a7a6471b8cbb80a7a4a5fd4b172a50b8fe9ff80
januar-s <jan.schmidt@stud.fhgr.ch>
2024-05-06 16:10:19 +0200

    Selector ändert Zeit

--- js/script.js
+++ js/script.js
@@ -1,34 +1,12 @@
 // Diese Elemente will ich immer wieder verwenden
 // querySelector() -> #gibt für id und . für class
-const staedte = document.querySelector('#stadt_suche');
 const sonnenaufgang = document.querySelector('#sonnenaufgang');
 const sonnenstand = document.querySelector('#sonnenstand');
 const sonnenuntergang = document.querySelector('#sonnenuntergang');
+const body = document.querySelector('body');
 let url = 'https://api.sunrise-sunset.org/json?lat=46.94798&lng=7.44743&date=today&tzid=Europe/Zurich';
 let formattedUrl = 'https://api.sunrise-sunset.org/json?lat=46.94798&lng=7.44743&date=today&tzid=Europe/Zurich&formatted=0';
 
-// // Initalisierung
-// init();
-
-// async function init() {
-//     let cocktails = await fetchData(url);
-//     console.log(cocktails.drinks[0].strDrink);
-//     cocktails.drinks.forEach(cocktail => {
-//         createItem(cocktail);
-//     });
-// };
-
-// async function search() {
-//     let searchValue = searchBox.value;
-//     let url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchValue}`;
-//     cocktailApp.innerHTML = '';
-//     let cocktails = await fetchData(url);
-//     console.log(cocktails.drinks[0].strDrink);
-//     cocktails.drinks.forEach(cocktail => {
-//         createItem(cocktail);
-//     });
-
-// }
 
 // Daten aus einer API holen
 async function fetchData(url) {
@@ -37,13 +15,64 @@ async function fetchData(url) {
         let data = await response.json();
         console.log(data);
         console.log(data.results.day_length);
+
+        if (data.results) {
+            document.querySelector('#sunriseTime').textContent = `${data.results.sunrise}`;
+            document.querySelector('#sunsetTime').textContent = `${data.results.sunset}`;
+            document.querySelector('#currentTime').textContent = moment().tz('Europe/Zurich').format('LT');
+        }
+
         return data;
+
     }
     catch (error) {
         console.log(error);
     }
 }
 fetchData(url);
+
+let staedte = [{
+    name: 'bern',
+    lat: 46.94798,
+    lng: 7.44743,
+    tzid: 'Europe/Zurich'
+},
+{
+    name: 'losangeles',
+    lat: 34.052235,
+    lng: -118.243683,
+    tzid: 'America/Los_Angeles'
+},
+{
+    name: 'tokio',
+    lat: 35.652832,
+    lng: 139.839478,
+    tzid: 'Asia/Tokyo'
+},
+{
+    name: 'kairo',
+    lat: 30.033333,
+    lng: 31.233334,
+    tzid: 'Africa/Cairo'
+},
+{
+    name: 'rio',
+    lat: -22.908333,
+    lng: -43.196388,
+    tzid: 'America/Sao_Paulo'
+}];
+
+document.addEventListener('DOMContentLoaded', function () {
+    const select = document.querySelector('#stadt_suche');
+
+    select.addEventListener('change', async function () {
+        let stadt = staedte.find(stadt => stadt.name === select.value);
+        let url = `https://api.sunrise-sunset.org/json?lat=${stadt.lat}&lng=${stadt.lng}&date=today&tzid=${stadt.tzid}`;
+        await fetchData(url);
+        document.querySelector('#currentTime').textContent = moment().tz(`${stadt.tzid}`).format('LT');
+        console.log(stadt.tzid);
+    });
+});
 
 function position() {
     const sonne = document.getElementById('sonne');
@@ -64,7 +93,9 @@ function position() {
                 const percentage = (elapsedMinutes / totalMinutes) * 100; // Prozentsatz des vergangenen Tages
                 
                 const angle = (percentage * 1.8) - 180; // Umrechnung in Grad (360 Grad / 200% = 1.8 Grad pro %)
+                console.log(angle);
                 moveSonne(angle);
+                dawn(angle);
             })
             .catch(error => console.error('Error fetching sun position:', error));
 
@@ -77,27 +108,13 @@ function position() {
         sonne.style.left = `${x}px`;
         sonne.style.top = `${y}px`;
     } moveSonne(180);
+
+    function dawn(angle) {
+        if (angle + 180 < 10 || angle + 180 > 170) {
+            body.style.background = 'linear-gradient(180deg, #FFC107 0%, #FFC107 50%, #FFEB3B 50%, #FFEB3B 100%)';
+        } else {
+            body.style.background = 'linear-gradient(180deg, #FFC107 0%, #FFEB3B 50%, #FFEB3B 100%)';
+        }        
+    };
 }
 position();
-
-
-async function fetchData(url) {
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
-        console.log(data);
-
-        if (data.results) {
-            document.querySelector('#sunriseTime').textContent = `${data.results.sunrise}`;
-            document.querySelector('#sunsetTime').textContent = `${data.results.sunset}`;
-            document.querySelector('#currentTime').textContent = moment().format('LT');
-        }
-
-        return data;  // Diese Zeile bleibt innerhalb des try-Blocks
-    } catch (error) {
-        console.error(error);
-    }
-
-}
-
-
